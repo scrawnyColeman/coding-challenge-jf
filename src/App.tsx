b4f6ea8d9040bc93287b5d8955d5cadb1b0a3638
scrawnycoleman <thomashutchinson450@gmail.com>
2024-07-01 18:02:54 +0100

    creates base view

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/table";
+
+import { records } from "./data";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="container mx-auto">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Investor Name</TableHead>
+          </TableRow>
+          <TableBody>
+            {records.map((record) => {
+              return (
+                <TableRow>
+                  <TableCell>{record.name}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </TableHeader>
+      </Table>
+    </div>
+  );
 }
